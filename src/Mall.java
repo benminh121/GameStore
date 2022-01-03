991c2d8824d2307280b0be68192bd49c4fcdb3b5
Minh Nguyen <nguyenthaiminh1201@gmail.com>
2022-01-03 15:06:34 +0700

    edit Mall class

--- src/Mall.java
+++ src/Mall.java
@@ -7,64 +7,78 @@ public class Mall implements Runnable {
     synchronized public void run() {
         Scanner sc = new Scanner(System.in);
         System.out.println(" \t\t\t################# Welcome to Smart Store #################");
-        System.out.println("Are you a customer YES/NO");
+        System.out.print("Are you a customer YES/NO: ");
         String cus = sc.next();
         while (true) {
-            switch (cus) {
+            // Choose customer or admin
+            switch (cus.toUpperCase()) {
                 case "YES":
                     Customer obj = new Customer();
-                    System.out.println("PS\nXbox\nNintendo");
-                    String ch = sc.next();
-
-                    obj.show(ch);
+                    // Choose platform
+                    System.out.print("Choose platform game: ");
+                    System.out.println("\nP for PS\tX for Xbox\tN for Nintendo Switch");
+                    String pf = sc.next();
+                    obj.show(pf);
                     break;
                 case "NO":
                     boolean isAdmin = checkAdmin();
                     if (isAdmin) {
-
                         Owner obj1;
+                        // Admin can add or remove game in store
                         System.out.println("Do you Want to Add or Remove");
-                        ch = sc.next();
+                        String ch = sc.next();
                         if (ch.equals("Add")) {
-                            System.out.println("Fill product details below:");
+                            // Add item - admin must enter all information of game DVD
+                            System.out.println("Fill game details below:");
+                            // Choose platform
                             System.out.println("Enter platform :\nP - Playstaion \nX - Xbox \nN - Nintendo Switch");
                             char platform = sc.next().charAt(0);
+                            // Enter game id
                             System.out.println("Enter game id");
                             int gameID = sc.nextInt();
+                            // Enter game title
                             System.out.println("Enter game title");
                             String title = sc.next();
+                            // Enter game price
                             System.out.println("Enter game price");
                             double price = sc.nextDouble();
+                            // Enter game description
                             System.out.println("Enter game description");
                             String description = sc.next();
+                            // Enter video
                             System.out.println("Enter game video");
                             String video = sc.next();
+                            // Enter cover
                             System.out.println("Enter game cover");
                             String cover = sc.next();
+                            // Enter image
                             System.out.println("Enter game image1");
                             String image1 = sc.next();
+                            // Enter Available or Unavailable
                             System.out.println("Enter game condition");
                             String condition = sc.next();
+                            // Enter Discount
                             System.out.println("Enter game discount");
                             double discount = sc.nextDouble();
-
+                            // Constructor add game in Owner class
                             obj1 = new Owner("add", platform, gameID, title, price, description, video, cover,
-                            image1, condition, discount);
+                                                image1, condition, discount);
+                        // Constructor remove game
                         } else if (ch.equals("Remove")) {
-                            System.out.println("Enter platform ");
+                            System.out.print("Enter platform P X N: ");
                             char platform=sc.next().charAt(0);
                             obj1 = new Owner("remove", platform, 21, "title", 4.3, "description", "video", "cover",
                             "image1", "condition", 2.3); // We enter random to skip constructor
                         }
                     } else {
-                        System.out.println("Sorry !Wrong Password or USer ID\n Try again");
+                        System.out.println("Sorry !Wrong Password or User ID\n Try again");
                     }
                     break;
                 default:
                     System.out.println("Wrong choice");
             }
             System.out.println("Do you want to exit the store ? YES / NO");
-            if (sc.next().equals("YES"))
+            if (sc.next().toUpperCase().equals("YES"))
                 break;
         }
         if (cus.equals("YES")) {
@@ -72,15 +86,14 @@ public class Mall implements Runnable {
             obj.displayBill();
         }
         System.out.println("Thank You , Visit Again");
-
+        sc.close();
     }
 
     boolean checkAdmin() {
 
-        File f = new File("admin.txt");
-        // System.out.println(new File("admin.txt").getAbsolutePath());
+        File f = new File("src/admin.txt");
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter id");
+        System.out.print("Enter id: ");
         String id = sc.next();
         // System.out.println("Enter Password");
         // String password=sc.next();
@@ -96,9 +109,10 @@ public class Mall implements Runnable {
         // System.out.println("Error "+e);
         // }
         try {
-            File inputFile = new File("admin.txt");
+            File inputFile = new File("src/admin.txt");
             BufferedReader reader = new BufferedReader(new FileReader(inputFile));
             String str = reader.readLine();
+            // In file admin.txt has format {id pass}
             str.trim();
             int firstgap = str.indexOf(" ");
             if (id.equals(str.substring(0, firstgap))) {
